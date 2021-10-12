1ec5e09449ee59df07b33a48b615b71294ba1393
gccornejo441 <gcornejo441@gmail.com>
2021-10-11 18:53:55 -0700

    fixing apos

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ import headShot from '../public/images/gabesHeadShot.jpg'
 
 const Home: NextPage = () => {
   return (
-    <div>
+    <>
       <Head>
         <title>Gabe Cornejo</title>
       </Head>
@@ -15,14 +15,14 @@ const Home: NextPage = () => {
         <div className="text-center flex justify-center mt-12 md:mt-36">
           <div className="px-10 md:px-10 md:w-1/2">
             <Image className="rounded-full" alt="Profile Picture" src={headShot} height={180} width={180} />
-            <h1 className="text-2xl text-mac-purple font-bold pb-10">Hi, I&aposm Gabe.</h1>
+            <h1 className="text-2xl text-mac-purple font-bold pb-10">Hi, I&apos;m Gabe.</h1>
             <p className="text-lg text-mac-purple font-bold mb-12">
-              I&aposm a web developer based in California.  I&aposm currently working away at developing awesome web apps.  In my free time I like to spend time with my family and work on side projects.
+              I&apos;m a web developer based in California.  I&apos;m currently working away at developing awesome web apps.  In my free time I like to spend time with my family and work on side projects.
             </p>
           </div>
         </div>
       </Layout>
-    </div>
+    </>
   )
 }
 
